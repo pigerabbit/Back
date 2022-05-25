bac2950eba987565dedbcd4b6756db8ccb7cec19
@eikjae <eikjae@gmail.com>
2022-05-25 14:17:17 +0900

    modify: user Service 완성 feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -1,38 +1,87 @@
-import { User } from "../db"; // from을 폴더(db) 로 설정 시, 디폴트로 index.js 로부터 import함.
+import { User } from "../db";
 import bcrypt from "bcrypt";
 import crypto from "crypto";
 import jwt from "jsonwebtoken";
+import sendMail from "../utils/send-mail";
+import { AuthEmail } from "../db";
+import { getRequiredInfoFromData } from "../utils/user";
 
 class userService {
-  static async addUser({ name, email, password }) {
-    // 이메일 중복 확인
-    const user = await User.findByEmail({ email });
-    if (user) {
-      const errorMessage =
-        "이 이메일은 현재 사용중입니다. 다른 이메일을 입력해 주세요.";
-      return { errorMessage };
-    }
+  static async addUser({
+    id,
+    name,
+    email,
+    password,
+    location,
+    distance,
+    business,
+    address,
+    type,
+    imageLink,
+  }) {
+    //일반회원가입일때
+    if (type === "sogongx2") {
+      const emailExists = await User.isEmailExists({ email, type });
+      //일반회원중에서 이메일이 존재하는지 체크
+      //   console.log(emailExists);
+      if (emailExists) {
+        const errorMessage =
+          "이 이메일은 현재 가입이력이 있는 이메일입니다. 다른 이메일을 이용해주세요.";
+        return { errorMessage };
+      }
+      //이메일이 인증완료된 이메일인지 체크
+      const authEmailExists = await AuthEmail.isAuthenticated({ email });
+      //   console.log(authEmailResult);
+      if (!authEmailExists) {
+        const errorMessage =
+          "인증이 완료되지 않은 이메일입니다.인증번호 발급후 인증을 완료해주세요.";
+        return { errorMessage };
+      }
 
+      imageLink = process.env.initial;
+    }
     // 비밀번호 해쉬화
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // id 는 유니크 값 부여
-    const id = crypto.randomUUID();
-    const newUser = { id, name, email, password: hashedPassword };
+    if (!id) {
+      id = crypto.randomUUID();
+    }
+    const newUser = {
+      id,
+      name,
+      email,
+      password: hashedPassword,
+      location,
+      distance,
+      business,
+      type,
+      imageLink,
+    };
+    //소셜로그인시 회원가입이 자동으로될때 없는 성분이 있기때문에 없는 성분들을 삭제
+    Object.keys(newUser).forEach((key) => {
+      if (newUser[key] === undefined) {
+        delete newUser[key];
+      }
+    });
 
     // db에 저장
     const createdNewUser = await User.create({ newUser });
-    createdNewUser.errorMessage = null; // 문제 없이 db 저장 완료되었으므로 에러가 없음.
+    const resultUser = getRequiredInfoFromData(createdNewUser);
 
-    return createdNewUser;
+    return resultUser;
   }
 
-  static async getUser({ email, password }) {
+  static async getUser({ email, password, type }) {
     // 이메일 db에 존재 여부 확인
-    const user = await User.findByEmail({ email });
+    const user = await User.findByEmail({ email, type });
     if (!user) {
       const errorMessage =
         "해당 이메일은 가입 내역이 없습니다. 다시 한 번 확인해 주세요.";
+      return { errorMessage };
+    }
+    if (user.deleted === true) {
+      const errorMessage = "해당 계정은 이미 탈퇴하였습니다.";
       return { errorMessage };
     }
 
@@ -53,18 +102,8 @@ class userService {
     const token = jwt.sign({ user_id: user.id }, secretKey);
 
     // 반환할 loginuser 객체를 위한 변수 설정
-    const id = user.id;
-    const name = user.name;
-    const description = user.description;
-
-    const loginUser = {
-      token,
-      id,
-      email,
-      name,
-      description,
-      errorMessage: null,
-    };
+    const loginUser = getRequiredInfoFromData(user);
+    loginUser.token = token;
 
     return loginUser;
   }
@@ -83,33 +122,19 @@ class userService {
       const errorMessage = "가입 내역이 없습니다. 다시 한 번 확인해 주세요.";
       return { errorMessage };
     }
-
-    // 업데이트 대상에 name이 있다면, 즉 name 값이 null 이 아니라면 업데이트 진행
-    if (toUpdate.name) {
-      const fieldToUpdate = "name";
-      const newValue = toUpdate.name;
-      user = await User.update({ user_id, fieldToUpdate, newValue });
+    if (user.deleted === true) {
+      const errorMessage = "해당 계정은 이미 탈퇴하였습니다.";
+      return { errorMessage };
     }
+    Object.keys(toUpdate).forEach((key) => {
+      if (toUpdate[key] === undefined || toUpdate[key] === null) {
+        delete toUpdate[key];
+      }
+    });
 
-    if (toUpdate.email) {
-      const fieldToUpdate = "email";
-      const newValue = toUpdate.email;
-      user = await User.update({ user_id, fieldToUpdate, newValue });
-    }
-
-    if (toUpdate.password) {
-      const fieldToUpdate = "password";
-      const newValue = toUpdate.password;
-      user = await User.update({ user_id, fieldToUpdate, newValue });
-    }
-
-    if (toUpdate.description) {
-      const fieldToUpdate = "description";
-      const newValue = toUpdate.description;
-      user = await User.update({ user_id, fieldToUpdate, newValue });
-    }
-
-    return user;
+    const updatedUser = await User.updateAll({ user_id, setter: toUpdate });
+    const resultUser = getRequiredInfoFromData(updatedUser);
+    return resultUser;
   }
 
   static async getUserInfo({ user_id }) {
@@ -121,8 +146,65 @@ class userService {
         "해당 이메일은 가입 내역이 없습니다. 다시 한 번 확인해 주세요.";
       return { errorMessage };
     }
+    if (user.deleted === true) {
+      const errorMessage = "해당 계정은 이미 탈퇴하였습니다.";
+      return { errorMessage };
+    }
 
-    return user;
+    const resultUser = getRequiredInfoFromData(user);
+    return resultUser;
+  }
+
+  static async checkPassword({ user_id, password }) {
+    const user = await User.findById({ user_id });
+    if (!user) {
+      const errorMessage =
+        "해당 유저는 가입 내역이 없습니다. 다시 한 번 확인해 주세요.";
+      return { errorMessage };
+    }
+    if (user.deleted === true) {
+      const errorMessage = "해당 계정은 이미 탈퇴하였습니다.";
+      return { errorMessage };
+    }
+    const correctPasswordHash = user.password;
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      correctPasswordHash
+    );
+
+    if (!isPasswordCorrect) {
+      const errorMessage = "비밀번호를 다시 한 번 확인해 주세요.";
+      return { errorMessage };
+    }
+    return true;
+  }
+
+  static async nodeMailer({ email, name, password }) {
+    const message = sendMail(
+      email,
+      "소공소공 임시 비밀번호입니다.",
+      `안녕하세요 ${name}님, 임시 비밀번호는: ${password} 입니다. 로그인 후 비밀번호를 꼭 변경해주세요!`
+    );
+
+    return message;
+  }
+
+  static async deleteUser({ user_id }) {
+    const user = await User.findById({ user_id });
+
+    // db에서 찾지 못한 경우, 에러 메시지 반환
+    if (!user || user === null) {
+      const errorMessage = "해당 유저가 존재하지 않습니다.";
+      return { errorMessage };
+    }
+    if (user.deleted === true) {
+      const errorMessage = "해당 계정은 이미 탈퇴하였습니다.";
+      return { errorMessage };
+    }
+    const setter = { deleted: true };
+    const deletedUser = await User.updateAll({ user_id, setter });
+
+    return deletedUser;
   }
 }
 
