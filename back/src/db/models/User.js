0441cf7c3288cb5a1effe9c679e50b6944e203b4
@eikjae <eikjae@gmail.com>
2022-05-25 14:22:44 +0900

    modify: User model 수정 feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- back/src/db/models/User.js
+++ back/src/db/models/User.js
@@ -1,38 +1,56 @@
 import { UserModel } from "../schemas/user";
 
-class User {
+export class User {
   static async create({ newUser }) {
     const createdNewUser = await UserModel.create(newUser);
     return createdNewUser;
   }
 
-  static async findByEmail({ email }) {
-    const user = await UserModel.findOne({ email });
-    return user;
-  }
-
   static async findById({ user_id }) {
-    const user = await UserModel.findOne({ id: user_id });
+    const user = await UserModel.findOne({ id: user_id }).lean();
+
     return user;
   }
 
   static async findAll() {
-    const users = await UserModel.find({});
+    //회원탈퇴한 유저는 제외
+    const users = await UserModel.find(
+      { deleted: false },
+      {
+        _id: false,
+        id: true,
+        email: true,
+        name: true,
+        location: true,
+        distance: true,
+        business: true,
+        address: true,
+        imageLink: true,
+      }
+    );
     return users;
   }
 
-  static async update({ user_id, fieldToUpdate, newValue }) {
-    const filter = { id: user_id };
-    const update = { [fieldToUpdate]: newValue };
-    const option = { returnOriginal: false };
-
+  static async updateAll({ user_id, setter }) {
     const updatedUser = await UserModel.findOneAndUpdate(
-      filter,
-      update,
-      option
+      { id: user_id },
+      { $set: setter },
+      { returnOriginal: false }
     );
     return updatedUser;
   }
-}
 
-export { User };
+  static async findByEmail({ email, type }) {
+    const user = await UserModel.findOne({ email, type });
+    return user;
+  }
+
+  static async deleteById({ user_id }) {
+    const user = await UserModel.deleteOne({ id: user_id });
+    return user;
+  }
+
+  static async isEmailExists({ email, type }) {
+    return await UserModel.exists({ email, type });
+  }
+}
