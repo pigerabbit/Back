af81c7f312b96e980dd7b65c64e09dccac82fe87
@eikjae <eikjae@gmail.com>
2022-05-25 14:04:30 +0900

    modify: user router 완성 feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -1,9 +1,84 @@
 import is from "@sindresorhus/is";
 import { Router } from "express";
 import { login_required } from "../middlewares/login_required";
-import { userService } from "../services/userService";
+import { userAuthService } from "../services/userService";
+import generateRandomPassword from "../utils/generate-random-password";
+import { User } from "../db";
+import { authEmailService } from "../services/authEmailService";
+import bcrypt from "bcrypt";
 
 const userRouter = Router();
+
+userAuthRouter.get("/user/checkEmail/:email", async function (req, res, next) {
+  const email = req.params.email;
+  const type = "sogongx2";
+  const user = await User.findByEmail({ email, type });
+  const result = { status: 0 };
+  if (user) {
+    result.status = 1;
+    res.json(result);
+  } else {
+    res.json(result);
+  }
+});
+
+userAuthRouter.post(
+  "/user/authEmail/:email/activateKey",
+  async function (req, res, next) {
+    try {
+      const email = req.params.email;
+      const authEmail = await authEmailService.addAuthEmail({ email });
+      if (authEmail) {
+        //추후 authEmail이 존재하면 성공한 것이므로 status:true로 res.json을 내보낼 것
+        res.send("인증키가 해당이메일로 발송되었습니다.");
+      } else {
+        res.json({ status: false });
+      }
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+userAuthRouter.post(
+  "/user/authEmail/:email/activate",
+  async function (req, res, next) {
+    try {
+      const userKey = req.body.activateKey;
+      const email = req.params.email;
+      const authEmail = await authEmailService.activateAuthEmail({
+        email,
+        userKey,
+      });
+      if (authEmail.errorMessage) {
+        throw new Error(authEmail.errorMessage);
+      }
+      res.json(authEmail);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+userAuthRouter.post(
+  "/user/authEmail/:email/activate",
+  async function (req, res, next) {
+    try {
+      const userKey = req.body.activateKey;
+      const email = req.params.email;
+      const authEmail = await authEmailService.activateAuthEmail({
+        email,
+        userKey,
+      });
+      if (authEmail.errorMessage) {
+        throw new Error(authEmail.errorMessage);
+      }
+      res.json(authEmail);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
 
 /**
  *  @swagger
@@ -110,12 +185,27 @@ userRouter.post("/users", async function (req, res, next) {
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
+    const passwordCheck = req.body.passwordCheck;
+    const distance = req.body.distance;
+    const business = req.body.business;
+    const type = "sogongx2";
+    const address = req.body.address;
+    const location = req.body.location;
+
+    if (password !== passwordCheck) {
+      throw new Error("password와 passwordCheck의 값이 일치하지 않습니다.");
+    }
 
     // 위 데이터를 유저 db에 추가하기
     const newUser = await userService.addUser({
       name,
       email,
       password,
+      location,
+      address,
+      distance,
+      business,
+      type,
     });
 
     if (newUser.errorMessage) {
@@ -134,8 +224,11 @@ userRouter.post("/user/login", async function (req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
+    // 가입 유형이 소공소공인 회원에 한해서만 로그인서비스 제공. 다른 타입은 자동로그인 구현
+    const type = "sogongx2";
+
     // 위 데이터를 이용하여 유저 db에서 유저 찾기
-    const user = await userService.getUser({ email, password });
+    const user = await userService.getUser({ email, password, type });
 
     if (user.errorMessage) {
       throw new Error(user.errorMessage);
@@ -183,13 +276,19 @@ userRouter.put("/users/:id", login_required, async function (req, res, next) {
   try {
     // URI로부터 사용자 id를 추출함.
     const user_id = req.params.id;
-    // body data 로부터 업데이트할 사용자 정보를 추출함.
-    const name = req.body.name ?? null;
-    const email = req.body.email ?? null;
-    const password = req.body.password ?? null;
-    const description = req.body.description ?? null;
 
-    const toUpdate = { name, email, password, description };
+    if (user_id != req.currentUserId) {
+      throw new Error("다른 소유자의 소유물을 변경할 권한이 없습니다.");
+    }
+
+    // body data 로부터 업데이트할 사용자 정보를 추출함.
+    // name, location, distance, business, address만 변경 가능, 비밀번호는 다른 라우터를 사용
+    const name = req.body.name ?? null;
+    const location = req.body.location ?? null;
+    const distance = req.body.distance ?? null;
+    const business = req.body.business ?? null;
+    const address = req.body.address ?? null;
+    const toUpdate = { name, location, distance, business, address };
 
     // 해당 사용자 아이디로 사용자 정보를 db에서 찾아 업데이트함. 업데이트 요소가 없을 시 생략함
     const updatedUser = await userService.setUser({ user_id, toUpdate });
@@ -207,25 +306,129 @@ userRouter.put("/users/:id", login_required, async function (req, res, next) {
 userRouter.get("/users/:id", login_required, async function (req, res, next) {
   try {
     const user_id = req.params.id;
-    const currentUserInfo = await userService.getUserInfo({ user_id });
+    const userInfo = await userService.getUserInfo({ user_id });
 
-    if (currentUserInfo.errorMessage) {
-      throw new Error(currentUserInfo.errorMessage);
+    if (userInfo.errorMessage) {
+      throw new Error(userInfo.errorMessage);
     }
 
-    res.status(200).send(currentUserInfo);
+    res.status(200).send(userInfo);
   } catch (error) {
     next(error);
   }
 });
 
-// jwt 토큰 기능 확인용, 삭제해도 되는 라우터임.
-userRouter.get("/afterlogin", login_required, function (req, res, next) {
-  res
-    .status(200)
-    .send(
-      `안녕하세요 ${req.currentUserId}님, jwt 웹 토큰 기능 정상 작동 중입니다.`
-    );
-});
+userAuthRouter.put(
+  "/users/:id/changePassword",
+  login_required,
+  async function (req, res, next) {
+    try {
+      const user_id = req.params.id;
+      // body data 로부터 업데이트할 사용자 정보를 추출함.
+      if (user_id != req.currentUserId) {
+        throw new Error("다른 소유자의 소유물을 변경할 권한이 없습니다.");
+      }
+      const currentPassword = req.body.currentPassword;
+
+      const checkPassword = await userAuthService.checkPassword({
+        user_id,
+        password: currentPassword,
+      });
+
+      if (checkPassword.errorMessage) {
+        throw new Error(checkPassword.errorMessage);
+      }
+
+      const newPassword = req.body.newPassword;
+      const hashedPassword = await bcrypt.hash(newPassword, 10);
+      const toUpdate = { password: hashedPassword };
+
+      const updatedUser = await userAuthService.setUser({
+        user_id,
+        toUpdate,
+      });
+      if (updatedUser.errorMessage) {
+        throw new Error(updatedUser.errorMessage);
+      }
+
+      res.status(200).send("비밀번호가 변경되었습니다.");
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+userAuthRouter.post(
+  "/users/:id/resetPassword",
+  login_required,
+  async function (req, res, next) {
+    try {
+      const user_id = req.params.id;
+      if (user_id != req.currentUserId) {
+        throw new Error("다른 소유자의 소유물을 변경할 권한이 없습니다.");
+      }
+      //추후 service단으로 빠질 예정입니다.
+      const user = await User.findById({ user_id });
+
+      if (!user) {
+        throw new Error("해당 메일로 가입된 사용자가 없습니다.");
+      }
+
+      const name = user.name;
+      const email = user.email;
+      //8개 무작위 숫자 string
+      const newPassword = generateRandomPassword();
+      console.log(newPassword);
+      //   console.log(newPassword);
+      const hashedPassword = await bcrypt.hash(newPassword, 10);
+      const toUpdate = { password: hashedPassword };
+      const updatedUser = await userAuthService.setUser({ user_id, toUpdate });
+
+      if (updatedUser.errorMessage) {
+        throw new Error(updatedUser.errorMessage);
+      }
+
+      await userAuthService.nodeMailer({ email, name, password: newPassword });
+
+      res.status(200).send("임시 비밀번호가 전송되었습니다.");
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+userAuthRouter.delete(
+  "/users/:id",
+  login_required,
+  async function (req, res, next) {
+    try {
+      const user_id = req.params.id;
+      const password = req.body.password;
+      if (user_id != req.currentUserId) {
+        throw new Error("다른 소유자의 소유물을 변경할 권한이 없습니다.");
+      }
+
+      const checkPassword = await userAuthService.checkPassword({
+        user_id,
+        password,
+      });
+
+      if (checkPassword.errorMessage) {
+        throw new Error(checkPassword.errorMessage);
+      }
+
+      const deletedUser = await userAuthService.deleteUser({
+        user_id,
+      });
+
+      if (deletedUser.errorMessage) {
+        throw new Error(deletedUser.errorMessage);
+      }
+      res.status(200).send("회원탈퇴를 완료하였습니다.");
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 export { userRouter };
