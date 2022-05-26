bff4ffc687a19863974d0aa776513c8c12b60b0e
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-27 00:18:13 +0900

    docs: add mock data

---
+++ back/src/test/category.js
@@ -0,0 +1,32 @@
+category = [
+  {
+    fruitVegetable: "과일/채소"
+  },
+  {
+    cereal: "견과/쌀"
+  },
+  {
+    seafood: "수산/해산/건어물"
+  },
+  {
+    meatEgg: "정육/달걀"
+  },
+  {
+    noddleSpice :"면/양념/오일"
+  },
+  {
+    drink: "생수/음료/우유"
+  },
+  {
+    coffeeAlcohol: "커피/주류"
+  },
+  {
+    living: "생활용품/리빙"
+  },
+  {
+    kitchen: "주방용품"
+  },
+  {
+    other: "기타"
+  },
+]
