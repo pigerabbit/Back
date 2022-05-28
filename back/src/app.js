71fc35e62c75f61cc60f45944025513d3ca32e2c
Jiwoo An <jiyouan9@gmail.com>
2022-05-29 02:39:33 +0900

    feat: POST, GET /posts

--- back/src/app.js
+++ back/src/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser")
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 import { userAuthRouter } from "./routers/userRouter";
 import { productRouter } from "./routers/productRouter";
+import { postRouter } from "./routers/postRouter";
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 // router, service 구현 (userAuthRouter는 맨 위에 있어야 함.)
 app.use(userAuthRouter);
 app.use(productRouter);
+app.use(postRouter);
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
