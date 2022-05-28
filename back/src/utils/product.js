d75e8cd54a135d521d52f4c289e210cd2f507dc5
Jiwoo An <jiyouan9@gmail.com>
2022-05-28 15:43:53 +0900

    feat: product return data format

---
+++ back/src/utils/product.js
@@ -0,0 +1,23 @@
+export function getRequiredInfoFromProductData(data) {
+  return {
+    userId: data.userId,
+    id: data.id,
+    images: data.images,
+    category: data.category,
+    name: data.name,
+    description: data.description,
+    descriptionImg: data.descriptionImg,
+    price: data.price,
+    salePrice: data.salePrice,
+    discountRate: data.discountRate,
+    minPurchaseQty: data.minPurchaseQty,
+    maxPurchaseQty: data.maxPurchaseQty,
+    views: data.views,
+    shippingFee: data.shippingFee,
+    shippingFeeCon: data.shippingFeeCon,
+    detail: data.detail,
+    detailImg: data.detailImg,
+    shippingInfo: data.shippingInfo,
+    policy: data.policy,
+  };
+}
