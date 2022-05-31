02cf125f4ba5de864adb8d87169ab00e45329da2
Jiwoo An <jiyouan9@gmail.com>
2022-06-01 01:56:26 +0900

    modify: change to  business auth router

--- back/src/app.js
+++ back/src/app.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 import { userRouter } from "./routers/userRouter";
+import { businessAuthRouter } from "./routers/businessAuthRouter";
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.get("/", (req, res) => {
 
 // router, service 구현 (userRouter는 맨 위에 있어야 함.)
 app.use(userRouter);
+app.use(businessAuthRouter);
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
