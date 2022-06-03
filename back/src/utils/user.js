cde03ddcaf9edce6a8d7a157f2ef4dad9a49ff4e
Jiwoo An <jiyouan9@gmail.com>
2022-06-03 23:23:20 +0900

    modify: return data style

--- back/src/utils/user.js
+++ back/src/utils/user.js
@@ -1,16 +1,32 @@
 export function getRequiredInfoFromData(data) {
-  return {
-    id: data.id,
-    email: data.email,
-    name: data.name,
-    address: data.address,
-    nickname: data.nickname,
-    seller: data.seller,
-    business: data.business,
-    location: data.location,
-    distance: data.distance,
-    type: data.type,
-    imageLink: data.imageLink,
-    reportedBy: data.reportedBy,
-  };
+  if (data.seller) {
+    return {
+      id: data.id,
+      email: data.email,
+      name: data.name,
+      address: data.address,
+      nickname: data.nickname,
+      seller: data.seller,
+      business: data.business,
+      location: data.location,
+      distance: data.distance,
+      type: data.type,
+      imageLink: data.imageLink,
+      reportedBy: data.reportedBy,
+    };
+  } else {
+    return {
+      id: data.id,
+      email: data.email,
+      name: data.name,
+      address: data.address,
+      nickname: data.nickname,
+      seller: data.seller,
+      location: data.location,
+      distance: data.distance,
+      type: data.type,
+      imageLink: data.imageLink,
+      reportedBy: data.reportedBy,
+    };
+  }
 }
