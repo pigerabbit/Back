39d4195f6df2d56d96da936db0553938e6cdaa76
@eikjae <eikjae@gmail.com>
2022-06-04 14:13:44 +0900

    PUT toggle/searchWord/:searchWord 관련 작업 완료 feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- back/src/db/schemas/toggle.js
+++ back/src/db/schemas/toggle.js
@@ -15,6 +15,9 @@ const ToggleSchema = new Schema({
   products: {
     type: [String],
   },
+  searchWords: {
+    type: [String],
+  },
   deleted: {
     type: Boolean,
     default: false,
--- back/src/routers/toggleRouter.js
+++ back/src/routers/toggleRouter.js
@@ -71,6 +71,31 @@ toggleRouter.put(
   }
 );
 
+toggleRouter.put(
+  "/toggle/searchWord/:searchWord",
+  login_required,
+  async function (req, res, next) {
+    try {
+      const userId = req.currentUserId;
+      const searchWord = req.params.searchWord;
+
+      const toUpdate = { searchWord };
+      const updatedSearchWordInfo = await toggleService.setToggleSearchWord({
+        userId,
+        toUpdate,
+      });
+
+      if (updatedSearchWordInfo.errorMessage) {
+        throw new Error(updatedSearchWordInfo.errorMessage);
+      }
+
+      res.status(200).json(updatedSearchWordInfo);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 toggleRouter.get(
   "/toggle/groups",
   login_required,
