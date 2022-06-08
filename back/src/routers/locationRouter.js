babd392a1ca53e44c7dbfa8adce06bb8736cb4c3
@eikjae <eikjae@gmail.com>
2022-06-08 20:21:22 +0900

    feat: addressToXY.js 모듈 완성 feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- back/src/routers/locationRouter.js
+++ back/src/routers/locationRouter.js
@@ -1,21 +1,20 @@
 import { Router } from "express";
 import { login_required } from "../middlewares/login_required";
-import { locationRouter } from "../services/userService";
+import { locationService } from "../services/userService";
+import { addressToXY } from "../utils/addressToXY.js";
 
-const alertRouter = Router();
+const locationRouter = Router();
 
-alertRouter.post(
-  "/locations/",
+locationRouter.put(
+  "/locations",
   login_required,
   async function (req, res, next) {
     try {
-      const x = req.body.x;
-      const y = req.body.y;
-      const radius = req.body.radius;
-
+      const address = req.query.address;
+      const data = await addressToXY(address);
       const body = {
         success: true,
-        payload: alertList,
+        payload: data,
       };
 
       return res.status(200).json(body);
