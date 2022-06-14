e974bc0b07500d5ab99e9d677f0b5fb844130ea5
Jiwoo An <jiyouan9@gmail.com>
2022-06-14 23:18:25 +0900

    refact: location router controller 분리

---
+++ back/src/controllers/locationController.js
@@ -0,0 +1,20 @@
+import { addressToXY } from "../utils/addressToXY.js";
+
+class locationController { 
+  static async getLocation(req, res, next) { 
+    try {
+      const address = req.query.address;
+      const data = await addressToXY(address);
+      const body = {
+        success: true,
+        payload: data,
+      };
+
+      return res.status(200).json(body);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export { locationController };
