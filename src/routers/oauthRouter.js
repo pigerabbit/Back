335284cd82641df2f9a2026909d293e59b0766a2
@eikjae <eikjae@gmail.com>
2022-07-01 15:18:32 +0900

    modify: oauth 작업 진행 feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- src/routers/oauthRouter.js
+++ src/routers/oauthRouter.js
@@ -1,8 +1,24 @@
 import { Router } from "express";
 import { oauthController } from "../controllers/oauthController";
+import { oauthService } from "../services/oauthService";
 
 const oauthRouter = Router();
 
 oauthRouter.post("/oauth/login", oauthController.login);
+
+oauthRouter.get("/users/login/kakao", async (req, res, next) => {
+  try {
+    const code = req.query.code;
+
+    const user = await oauthService.upsertKakaoUser({ code });
+
+    // const redirect_uri = `http://localhost:3000/login/kakao?token=${user.token}`;
+    // 배포용으로 수정
+    const redirect_uri = `http://localhost:3000/login/kakao?token=${user.token}`;
+    res.status(200).redirect(redirect_uri);
+  } catch (err) {
+    next(err);
+  }
+});
 
 export { oauthRouter };
--- src/services/oauthService.js
+++ src/services/oauthService.js
@@ -84,6 +84,57 @@ class oauthService {
 
     return loginUser;
   }
+
+  static async upsertKakaoUser({ code }) {
+    const KAKAO_CLIENT_id = "65f9f7bd7bfa363ccc6e500401314239";
+    // const KAKAO_REDIRECT_URL = "http://localhost:5000/users/login/kakao";
+    // 배포용으로 수정
+    const KAKAO_REDIRECT_URL =
+      "http://elice-kdt-ai-4th-team03.elicecoding.com:5000/users/login/kakao";
+    //카카오 토큰 받기
+    const ret = await axios.post(
+      `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${KAKAO_CLIENT_id}&redirect_uri=${KAKAO_REDIRECT_URL}&code=${code}`
+    );
+
+    const kakaoToken = ret.data.access_token;
+
+    //카카오 유저정보 받기
+    const kakaoData = await axios.get(`https://kapi.kakao.com/v2/user/me`, {
+      headers: { Authorization: `Bearer ${kakaoToken}` },
+    });
+
+    // const userData = {
+    //   id: kakaoData.data.id,
+    //   email: kakaoData.data.kakao_account.email || "이메일 동의 안함",
+    //   name: kakaoData.data.kakao_account.profile.nickname,
+    //   password: "kakao",
+    // };
+    // let user;
+    // const isUserExist = await User.findById({ id: userData.id });
+    // if (isUserExist) {
+    //   //최초 로그인 아님, 디비 기존 정보 업데이트
+    //   user = await User.update({ id: userData.id, toUpdate: userData });
+    // } else {
+    //   //최초 로그인, 디비에 새로 생성
+    //   user = await User.create({ newUser: userData });
+    // }
+
+    // // 로그인 성공 -> JWT 웹 토큰 생성
+    // const secretKey = process.env.JWT_SECRET_KEY || "jwt-secret-key";
+    // const token = jwt.sign({ id: user.id }, secretKey);
+
+    // const loginUser = {
+    //   token,
+    //   id: user.id,
+    //   email: user.email,
+    //   name: user.name,
+    //   errorMessage: null,
+    // };
+
+    // return loginUser;
+
+    return kakaoData;
+  }
 }
 
 export { oauthService };
