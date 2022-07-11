6a8d419242aa909bfd007e63d9a2f689051fa704
JiwooAn <jiyouan9@gmail.com>
2022-07-12 08:24:37 +0900

    docs: 주석 추가

--- src/routers/locationRouter.js
+++ src/routers/locationRouter.js
@@ -5,6 +5,7 @@ import { locationController } from "../controllers/locationController";
 
 const locationRouter = Router();
 
+// 주소를 위도 경도로 변환해주는 API
 locationRouter.get(
   "/locations",
   login_required,
