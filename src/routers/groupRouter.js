80581ad7b848c1a248ecdf6eac2ad806b66beeac
eikjae <eikjae@gmail.com>
2022-07-25 02:06:58 +0900

    refact: groupRouter 주석 추가, 정렬, 순서 변경

--- src/routers/groupRouter.js
+++ src/routers/groupRouter.js
@@ -4,33 +4,11 @@ import { groupController } from "../controllers/groupController";
 
 const groupRouter = Router();
 
-groupRouter.post("/groups", login_required, groupController.createGroup);
-
-groupRouter.put(
-  "/groups/:groupId/quantity",
+// 공동구매 생성
+groupRouter.post(
+  "/groups",
   login_required,
-  groupController.updateQuantity
-);
-
-// 특정 아이디의 지불 여부 변경
-groupRouter.put(
-  "/groups/:groupId/payment",
-  login_required,
-  groupController.updatePayment
-);
-
-// 특정 아이디의 지불 여부 변경
-groupRouter.put(
-  "/groups/:groupId/review",
-  login_required,
-  groupController.updateReview
-);
-
-// 공동구매 정보 변경
-groupRouter.put(
-  "/groups/groupId/:groupId",
-  login_required,
-  groupController.updateGroup
+  groupController.createGroup
 );
 
 // 공동구매 아이디를 통해 공동구매 검색
@@ -40,7 +18,7 @@ groupRouter.get(
   groupController.getGroup
 );
 
-// 상품 아이디에 대한 모든 공동구매들을 반환하는 함수
+// 상품에 대한 모든 공동구매 검색
 groupRouter.get(
   "/groups/productId/:productId",
   login_required,
@@ -54,28 +32,14 @@ groupRouter.get(
   groupController.getGroupsByNumberInfo
 );
 
-// 내가 owner인 공동구매들 반환
+// 내가 공구장인 공동구매들 반환
 groupRouter.get(
   "/groups/manager/:boolean",
   login_required,
   groupController.getGroupsByIsManager
 );
 
-// 공동구매에 참가자 추가
-groupRouter.put(
-  "/groups/:groupId/participate/in",
-  login_required,
-  groupController.updateParticipateIn
-);
-
-// 공동구매 나가기
-groupRouter.put(
-  "/groups/:groupId/participate/out",
-  login_required,
-  groupController.updateParticipateOut
-);
-
-// 공동구매 아이디를 통해 공동구매 검색
+// 공동구매 state 정보 가져오기
 groupRouter.get(
   "/groups/:groupId/stateInfo",
   login_required,
@@ -89,20 +53,63 @@ groupRouter.get(
   groupController.getGroupsByRemainedTime
 );
 
-// 모집 인원이 적게 남은 순으로 오름차순 정렬
+// 모집 인원이 적게 남은 순으로 오름차순 정렬 (단, 3명 이하)
 groupRouter.get(
   "/groups/sort/remainedPersonnel",
   login_required,
   groupController.getGroupsByRemainedPersonnel
 );
 
-// 열린 공구가 가까운 순으로 오름차순 정렬
+// 열린 공구가 가까운 순으로 오름차순 정렬 (단, 5km 이내)
 groupRouter.get(
   "/groups/sort/locations",
   login_required,
   groupController.getGroupsByLocations
 );
 
+// 특정 아이디의 공동구매 수량 변경
+groupRouter.put(
+  "/groups/:groupId/quantity",
+  login_required,
+  groupController.updateQuantity
+);
+
+// 특정 아이디의 지불 여부 변경
+groupRouter.put(
+  "/groups/:groupId/payment",
+  login_required,
+  groupController.updatePayment
+);
+
+// 특정 아이디의 리뷰 여부 변경
+groupRouter.put(
+  "/groups/:groupId/review",
+  login_required,
+  groupController.updateReview
+);
+
+// 이외의 공동구매 정보 변경
+groupRouter.put(
+  "/groups/groupId/:groupId",
+  login_required,
+  groupController.updateGroup
+);
+
+// 공동구매 참여하기
+groupRouter.put(
+  "/groups/:groupId/participate/in",
+  login_required,
+  groupController.updateParticipateIn
+);
+
+// 공동구매 나가기
+groupRouter.put(
+  "/groups/:groupId/participate/out",
+  login_required,
+  groupController.updateParticipateOut
+);
+
+// 공동구매 닫기
 groupRouter.delete(
   "/groups/:groupId",
   login_required,
